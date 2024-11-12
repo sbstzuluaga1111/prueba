57d284ba252be5455632345b0d947e1396979a4e
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2024-11-12 14:58:56 -0500

    Cambios importantes y falta el filtro

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-    <div class="d-flex justify-center">
-      <h1>Fate</h1>
+    <div class="d-flex justify-center espacio">
+      <h1>{{ titulo }}</h1> <!-- Aquí mostramos el título -->
     </div>
     
     <div v-if="loading" class="loading-container d-flex justify-center align-center">
@@ -14,14 +14,13 @@
         :key="servant.id"
         cols="12"
         md="4"
-        lg="4"
+        lg="3"
       >
         <v-card class="servant-card">
           <v-img
             :src="getFaceUrl(servant)"
             alt="Servant Face"
-            height="300px"
-            contain
+            height="600px"
             class="servant-image"
           ></v-img>
           <v-card-title>{{ servant.name }}</v-card-title>
@@ -35,59 +34,5 @@
   </v-container>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      servants: [],
-      loading: true,
-    };
-  },
-  created() {
-    fetch("https://api.atlasacademy.io/export/NA/nice_servant.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.servants = data;
-        this.loading = false;
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        this.loading = false;
-      });
-  },
-  methods: {
-    getFaceUrl(servant) {
-      return servant.extraAssets?.charaGraph?.ascension[4] || servant.extraAssets?.charaGraph?.ascension[3] || servant.extraAssets?.charaGraph?.ascension[2] || servant.extraAssets?.charaGraph?.ascension[1];
-    },
-  },
-};
-</script>
-
-<style scoped>
-.v-container {
-  padding: 10px;
-}
-
-.loading-container {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(255, 255, 255, 0);
-}
-
-.servant-card {
-  max-width: 400px;
-  
-}
-
-.servant-image {
-  width: 400px;
-  height: 400px;
-  object-fit: cover;
-}
-</style>
+<script src="../scripts/HelloWorld.js"></script>
+<style src="../css/HelloWorld.css"></style>
